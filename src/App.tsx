f820adee0a3efb04ee03a522da53f3a20ac916b1
Maxwell Wang <90732327+mwang840@users.noreply.github.com>
2022-02-06 18:00:08 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ function App(): JSX.Element {
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
-                return Maxwell Wang
             </p>
         </div>
     );
