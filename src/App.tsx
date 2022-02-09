7aa2728eecc29c87c3a572c46113ffcd228d6733
Maxwell Wang <maxwang@udel.edu>
2022-02-09 16:47:05 -0500

    New changes for app.tss and app.css, also included both package.jsons.  Please enter the commit message for your changes. Lines starting

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
@@ -6,7 +7,36 @@ function App(): JSX.Element {
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
+                <h1>Published by a Sophmore Student</h1>
             </header>
+
+            <img
+                src="https://pm1.narvii.com/6991/10f3d752607b0b6a188c8cc72170a5cdc09c1aa5r1-1080-943v2_hq.jpg"
+                alt="Mr.Diktovitch needs rent!"
+                height="100"
+                width="100"
+            />
+            <p>Hello World</p>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+            <Container>
+                <Row>
+                    <Col>Bully Maguire</Col> <Col>Bully Garfield</Col>
+                </Row>
+            </Container>
+            <div
+                style={{
+                    width: "40px",
+                    height: "80px",
+                    backgroundColor: "yellow"
+                }}
+            ></div>
+            <ul>
+                <li>Tottenham</li>
+                <li>Chelsea</li>
+                <li>Arsenal</li>
+            </ul>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
