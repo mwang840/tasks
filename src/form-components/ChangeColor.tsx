06a0d1c73f490c8bd005ed99dc834e1a2cf2157a
Maxwell Wang <maxwang@udel.edu>
2022-03-14 15:26:38 -0400

    Temporary solved forums

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,43 @@
 import React, { useState } from "react";
+import { Form, FormLabel } from "react-bootstrap";
+type ChangeEvent = React.ChangeEvent<
+    HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+>;
+const COLORS = [
+    "red",
+    "blue",
+    "green",
+    "yellow",
+    "navy blue",
+    "orange",
+    "purple",
+    "black",
+    "white",
+    "brown"
+];
 
 export function ChangeColor(): JSX.Element {
+    const [choice, setChoice] = useState<string>(COLORS[0]);
     return (
         <div>
             <h3>Change Color</h3>
+            <div>
+                The current color is{""}
+                <span style={{ backgroundColor: choice, color: "white" }}>
+                    {choice}
+                </span>
+                {COLORS.map((color: string) => (
+                    <Form.Check
+                        key={color}
+                        inline
+                        type="radio"
+                        name="colors"
+                        onChange={(
+                            event: React.ChangeEvent<HTMLInputElement>
+                        ) => setChoice(event.target.value)}
+                    />
+                ))}
+            </div>
         </div>
     );
 }
--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,40 @@
 import React, { useState } from "react";
+import { Form, FormLabel } from "react-bootstrap";
 
 export function EditMode(): JSX.Element {
+    const [username, setUserName] = useState<string>("Your Name");
+    const [student, setIsStudent] = useState<boolean>(true);
+    const [editing, setIsEditing] = useState<boolean>(false);
+
+    function updateName(event: React.ChangeEvent<HTMLInputElement>) {
+        setUserName(event.target.value);
+    }
+
     return (
         <div>
             <h3>Edit Mode</h3>
+            <div>
+                <Form.Check
+                    type="switch"
+                    id="is-edit-mode"
+                    label="Editing:"
+                    checked={editing}
+                    onChange={() => setIsEditing(!editing)}
+                ></Form.Check>
+            </div>
+            <div>
+                {editing ? (
+                    <Form.Group controlId="formPersonName">
+                        <Form.Label>Name: </Form.Label>
+                        <Form.Control value={username} onChange={updateName} />
+                    </Form.Group>
+                ) : (
+                    <div>
+                        <span>{username}</span>
+                        {student ? " is a student" : " is not a student"}
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
