e970bc031e432abf12543eed313ee72f9e9c2dbf
Maxwell Wang <maxwang@udel.edu>
2022-03-17 05:18:31 -0400

    Added a json file

--- src/quizzer/Quizzer.tsx
+++ src/quizzer/Quizzer.tsx
@@ -1,4 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "react-bootstrap";
+export function ShowHideTasks(): JSX.Element {
+    const [visible, setVisible] = useState<boolean>(false);
+    return (
+        <div>
+            {visible && <div></div>}
+            <Button onClick={() => setVisible(!visible)}>Show/Hide</Button>
+        </div>
+    );
+}
 
 export function Quizzer(): JSX.Element {
     return <h3>Quizzer</h3>;
