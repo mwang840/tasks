9bf58ba4d097f448c05e064ca18e144895cf2e0b
Maxwell Wang <maxwang@udel.edu>
2022-03-22 16:25:26 -0400

    first embed for the jumpscare.tsx

--- src/quizzer/Jumpscares.tsx
+++ src/quizzer/Jumpscares.tsx
@@ -3,9 +3,9 @@ import React from "react";
 export function fnafJumpScared({ name }: { name: string }): JSX.Element {
     return (
         <iframe
-            width="642"
-            height="361"
-            src={`https://www.youtube.com/embed/${name}`}
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/DSZ9-eEn3Hc"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
