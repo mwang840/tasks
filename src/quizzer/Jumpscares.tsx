f9a8aeea6b6b9bc9704d75481c077b64afdfa830
Maxwell Wang <maxwang@udel.edu>
2022-03-27 21:53:23 -0400

    Added more to jumpscares

--- src/quizzer/Jumpscares.tsx
+++ src/quizzer/Jumpscares.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-export function fnafJumpScared({ name }: { name: string }): JSX.Element {
+export function GetJumpScared({ id }: { id: string }): JSX.Element {
     return (
         <iframe
             width="560"
             height="315"
-            src="https://www.youtube.com/embed/DSZ9-eEn3Hc"
+            src={`https://www.youtube.com/embed/${id}`}
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
