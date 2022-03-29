a60880e286dc0a8c3b388ba86c8ac885917b30fb
Maxwell Wang <maxwang@udel.edu>
2022-03-29 12:45:45 -0400

    made a modifyQuestionFile

---
+++ src/quizzer/ModifyQuestion.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import { Question, QuestionType } from "./interfaces/question";
+
+export function ModifyQuestion(): JSX.Element {
+    function publishQuestion(question: Question): Question {
+        const newQuestion = { ...question, published: !question.published };
+        return newQuestion;
+    }
+
+    function unpublishQuestion(question: Question): Question {
+        const newQuestion = { ...question, published: question.published };
+        return newQuestion;
+    }
+    return <div></div>;
+}
