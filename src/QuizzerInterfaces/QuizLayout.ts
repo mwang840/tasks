69c78fe12ff5be5272c3276e73f51d8aaebe36c4
Maxwell Wang <maxwang@udel.edu>
2022-03-29 19:21:01 -0400

    changed the interface to quizlayout better naming

--- src/QuizzerInterfaces/QuizLayout.ts
+++ src/QuizzerInterfaces/QuizLayout.ts
@@ -1,6 +1,6 @@
 import { QuizQuestions } from "./QuizQuestions";
 
-export interface Quiz {
+export interface QuizLayout {
     title: string; //quiz name
     description: string; //what quiz is on
     totalQuestions: number; //total Amt of questions
