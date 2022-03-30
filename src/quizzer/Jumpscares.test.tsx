add66ff798d8364f0e8b609abcf2bb8195a1ca7b
Maxwell Wang <maxwang@udel.edu>
2022-03-30 17:00:20 -0400

    fixed tests but not running yet

--- src/quizzer/Jumpscares.test.tsx
+++ src/quizzer/Jumpscares.test.tsx
@@ -3,10 +3,11 @@ import { render } from "@testing-library/react";
 import { GetJumpScared } from "./Jumpscares";
 
 describe("JumpScares Tests", () => {
+    test("The Jumpscare renders an each small attribute of a jumpscare", {});
     beforeEach(() => {
-        render(<GetJumpScared id={""} />);
+        render(<GetJumpScared id={"DSZ9 - eEn3Hc"} />);
     });
     test("The Jumpscare renders", () => {
-        expect(GetJumpScared({"DSZ9 - eEn3Hc"})).toBe(6);
+        expect("https://www.youtube.com/embed/DSZ9 - eEn3Hc");
     });
 });
