cca1672933cc28aac6a2a8b0cb278a64d77ced75
Maxwell Wang <maxwang@udel.edu>
2022-03-30 17:37:55 -0400

    set up see question file

---
+++ src/quizzer/SeeQuestion.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import { QuizQuestions } from "../QuizzerInterfaces/QuizQuestions";
+type ChangeEvent = React.ChangeEvent<
+    HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+>;
+
+export function SeeQuestion(): JSX.Element {}
