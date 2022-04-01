ab2761ef31a5eb96f30dc4f1b8d3e186a42e593a
Maxwell Wang <maxwang@udel.edu>
2022-04-01 15:50:35 -0400

    small changes

--- src/quizzer/SeeQuestion.tsx
+++ src/quizzer/SeeQuestion.tsx
@@ -6,6 +6,33 @@ type ChangeEvent = React.ChangeEvent<
     HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
 >;
 
-export function SeeQuestion(): JSX.Element {
-    return <div></div>;
+export function SeeQuestion({
+    question
+}: {
+    question: QuizQuestions;
+}): JSX.Element {
+    const [answerChoices, setanswerChoices] = useState<string>(
+        question.options[0]
+    );
+    const [answer, setAnswer] = useState<string>("");
+    const choices = question.options;
+    function checkAnswer(event: ChangeEvent) {
+        setAnswer(event.target.value);
+    }
+    function matchingAnswer(event: ChangeEvent) {
+        setanswerChoices(event.target.value);
+    }
+    //Organizes questions out
+    return (
+        <div>
+            <h2>{question.name}</h2>
+            <div>
+                <h3>{question.body}</h3>
+                <h4>{"Points" + question.points}</h4>
+                <h5>{question.type === "short-answer" ? <div>
+                    <Form.Group controlId = "short-answers only"></Form.Group>
+                </div>): ()}</h5>
+            </div>
+        </div>
+    );
 }
