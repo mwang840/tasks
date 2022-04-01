788f238730ab9613f9d8cbfb197c7aa63da51cdc
Maxwell Wang <maxwang@udel.edu>
2022-03-31 21:17:09 -0400

    made good imports

--- src/quizzer/Jumpscares.test.tsx
+++ src/quizzer/Jumpscares.test.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import { GetJumpScared } from "./Jumpscares";
+import "jest-dom/extend-expect";
+import "react-testing-library/cleanup-after-each";
 
 describe("JumpScares Tests", () => {
     test("The Jumpscare renders an each small attribute of a jumpscare", {});
