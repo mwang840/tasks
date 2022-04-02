0190f6927d6d96019e5b635a1e9285636faa0b77
Maxwell Wang <maxwang@udel.edu>
2022-04-01 21:26:04 -0400

    fixed jumpscare but need some more tests

--- src/quizzer/Jumpscares.test.tsx
+++ src/quizzer/Jumpscares.test.tsx
@@ -5,7 +5,7 @@ import "jest-dom/extend-expect";
 import "react-testing-library/cleanup-after-each";
 
 describe("JumpScares Tests", () => {
-    test("The Jumpscare renders an each small attribute of a jumpscare", {});
+    test("The Jumpscare renders an each small attribute of a jumpscare");
     beforeEach(() => {
         render(<GetJumpScared id={"DSZ9 - eEn3Hc"} />);
     });
