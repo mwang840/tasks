740b7629c419db45d8bb1893943d41022f111bcb
Maxwell Wang <maxwang@udel.edu>
2022-04-03 19:55:44 -0400

    changed layout for quiz

--- src/quizzer/ListQuiz.tsx
+++ src/quizzer/ListQuiz.tsx
@@ -1,26 +1,14 @@
 import React from "react";
 import { Stack } from "react-bootstrap";
-import { Question } from "../interfaces/question";
-import { SeeQuestion } from "./SeeQuestion";
+import { QuizLayout } from "../QuizzerInterfaces/QuizLayout";
+import { QuizView } from "./QuizView";
 
-export function QuestionList({
-    questions,
-    increasePoints,
-    showUnpub
-}: {
-    questions: Question[];
-    increasePoints: (addedP: number) => void;
-    showUnpub: boolean;
-}): JSX.Element {
+export function QuizList({ quizzes }: { quizzes: Quiz[] }): JSX.Element {
     return (
         <Stack gap={3}>
-            {questions.map((question: Question) => (
-                <div key={question.id} className="bg-light border m-2 p-2">
-                    <SeeQuestion
-                        question={question}
-                        increasePoints={increasePoints}
-                        showUnpub={showUnpub}
-                    ></SeeQuestion>
+            {quizzes.map((quiz: Quiz) => (
+                <div key={quiz.title}>
+                    <QuizView quiz={quiz}></QuizView>
                 </div>
             ))}
         </Stack>
