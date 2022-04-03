cee305db4312c82677fe69f0b9c6ccfcac9c84a9
Maxwell Wang <maxwang@udel.edu>
2022-04-03 19:57:56 -0400

    fixed layouts

--- src/quizzer/ListQuiz.tsx
+++ src/quizzer/ListQuiz.tsx
@@ -3,10 +3,10 @@ import { Stack } from "react-bootstrap";
 import { QuizLayout } from "../QuizzerInterfaces/QuizLayout";
 import { QuizView } from "./QuizView";
 
-export function QuizList({ quizzes }: { quizzes: Quiz[] }): JSX.Element {
+export function QuizList({ quizzes }: { quizzes: QuizLayout[] }): JSX.Element {
     return (
         <Stack gap={3}>
-            {quizzes.map((quiz: Quiz) => (
+            {quizzes.map((quiz: QuizLayout) => (
                 <div key={quiz.title}>
                     <QuizView quiz={quiz}></QuizView>
                 </div>
