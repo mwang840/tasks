7116ef5e28fb045579dbf6eacc1ba8ab178e6a92
Maxwell Wang <maxwang@udel.edu>
2022-04-03 14:43:01 -0400

    imputted change events and colums.

--- src/quizzer/ModifyQuestion.tsx
+++ src/quizzer/ModifyQuestion.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Question } from "../interfaces/question";
-import { Form, Button, Row, Col } from "react-bootstrap";
+import { Form, Button, Row, Col, Container } from "react-bootstrap";
 
 export function ModifyQuestion({
     question,
@@ -32,19 +32,88 @@ export function ModifyQuestion({
     function dontSave() {
         alterEdit();
     }
-
-    function publishQuestion(question: Question): Question {
-        const newQuestion = { ...question, published: !question.published };
-        return newQuestion;
-    }
-
-    function unpublishQuestion(question: Question): Question {
-        const newQuestion = { ...question, published: question.published };
-        return newQuestion;
-    }
     return (
-        <div>
-            <Form.Group controlId="question-layout"></Form.Group>
-        </div>
+        <Container>
+            <Row>
+                <Col>
+                    <Form.Group controlId="formQuestionBody" as={Row}>
+                        <Form.Label column sm={2}>
+                            Body:
+                        </Form.Label>
+                        <Col>
+                            <Form.Control
+                                value={bodies}
+                                onChange={(
+                                    event: React.ChangeEvent<HTMLInputElement>
+                                ) => setBodies(event.target.value)}
+                            />
+                        </Col>
+                    </Form.Group>
+                    <Form.Group controlId="formQuestionExpected" as={Row}>
+                        <Form.Label column sm={2}>
+                            Expected:
+                        </Form.Label>
+                        <Col>
+                            <Form.Control
+                                value={actual}
+                                onChange={(
+                                    event: React.ChangeEvent<HTMLInputElement>
+                                ) => setActual(event.target.value)}
+                            />
+                        </Col>
+                    </Form.Group>
+                    <Form.Check
+                        type="checkbox"
+                        id="is-Published-check"
+                        label="Publish?"
+                        checked={publish}
+                        onChange={(
+                            event: React.ChangeEvent<HTMLInputElement>
+                        ) => setPublish(event.target.checked)}
+                        style={{
+                            margin: "auto",
+                            width: "fit-content"
+                        }}
+                    />
+                    <Form.Group controlId="formQuestionPoints" as={Row}>
+                        <Form.Label column sm={2}>
+                            Points:
+                        </Form.Label>
+                        <Col>
+                            <Form.Control
+                                type="number"
+                                value={points}
+                                onChange={(
+                                    event: React.ChangeEvent<HTMLInputElement>
+                                ) =>
+                                    setPoints(parseInt(event.target.value) || 0)
+                                }
+                            />
+                        </Col>
+                    </Form.Group>
+                    <Button
+                        onClick={saveProgress}
+                        variant="success"
+                        className="me-4"
+                    >
+                        Save
+                    </Button>
+                    <Button
+                        onClick={dontSave}
+                        variant="warning"
+                        className="me-5"
+                    >
+                        Cancel
+                    </Button>
+                    <Button
+                        onClick={() => deleteQuestion(question.id)}
+                        variant="danger"
+                        className="me-8"
+                    >
+                        Delete
+                    </Button>
+                </Col>
+            </Row>
+        </Container>
     );
 }
