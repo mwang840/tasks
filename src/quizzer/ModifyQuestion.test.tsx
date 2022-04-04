1db795336584a22996fbd2968ec89a70a11c6889
Maxwell Wang <maxwang@udel.edu>
2022-04-03 20:00:22 -0400

    included the questions interface

---
+++ src/quizzer/ModifyQuestion.test.tsx
@@ -0,0 +1,7 @@
+import { ModifyQuestion } from "./ModifyQuestion";
+import "jest-dom/extend-expect";
+import "react-testing-library/cleanup-after-each";
+
+test("Testing Edit Question", () => {
+    expect(ModifyQuestion()).toEqual(1);
+});
